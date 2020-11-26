d5799b745655a894dc1564b5c2831e3dd281efa2
singiyeol <koin12000@naver.com>
2020-11-27 07:40:31 +0900

    2020.11.27 designing...

---
+++ app/src/main/java/com/example/switchcamera/SCActivity/SC_ImageEditActivity.java
@@ -0,0 +1,31 @@
+package com.example.switchcamera.SCActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.ImageView;
+
+import com.example.switchcamera.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SC_ImageEditActivity extends AppCompatActivity {
+
+    private ImageView sc_edit_imageView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sc_edit_activity);
+
+        sc_edit_imageView = findViewById(R.id.sc_edit_imageview);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        sc_edit_imageView.setImageURI(SC_MainActivity.galleryUri);
+    }
+}
--- app/src/main/java/com/example/switchcamera/SCActivity/SC_MainActivity.java
+++ app/src/main/java/com/example/switchcamera/SCActivity/SC_MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.switchcamera;
+package com.example.switchcamera.SCActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
@@ -14,6 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Bitmap;
@@ -26,18 +30,32 @@ import android.hardware.Camera.PictureCallback;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.example.switchcamera.CameraPreview;
+import com.example.switchcamera.JNIDriver;
+import com.example.switchcamera.R;
 import com.example.switchcamera.SCInterface.JNIListener;
 
-public class MainActivity extends AppCompatActivity implements JNIListener {
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class SC_MainActivity extends AppCompatActivity implements JNIListener {
     TextView tv;
     String str = "";
     JNIDriver mDriver;
     boolean mThreadRun = true;
     private static final String TAG = "CamTestActivity";
 
+    public static Bitmap capturedBitmap;
+    public static Uri galleryUri;
+
     private Camera mCamera;
+
+    private FrameLayout mainFrameLayout, cameraPreview;
     private CameraPreview mPreview;
     private ImageView capturedImageHolder;
+    private ImageButton captureButton;
+    private Button galleryButton;
+
 
     @Override
     protected  void onCreate(Bundle savedInstanceState){
@@ -48,14 +66,42 @@ public class MainActivity extends AppCompatActivity implements JNIListener {
         mDriver = new JNIDriver();
         mDriver.setListener(this);
         if(mDriver.open("/dev/sm9s5422_interrupt")<0){
-            Toast.makeText(MainActivity.this,"Driver Open Failed", Toast.LENGTH_SHORT).show();
+            Toast.makeText(SC_MainActivity.this,"Driver Open Failed", Toast.LENGTH_SHORT).show();
         }
-        capturedImageHolder = (ImageView) findViewById(R.id.captured_image);
 
         if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
             requestCameraPermission();
         }
+
+        mainFrameLayout = findViewById(R.id.main_framelayout);
+        cameraPreview = findViewById(R.id.camera_preview);
+
+        captureButton = (ImageButton) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCamera.takePicture(null, null, pictureCallback);
+            }
+        });
+
+        galleryButton = findViewById(R.id.button_gallery);
+        galleryButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, 101);
+            }
+        });
+
+
+        capturedImageHolder = (ImageView) findViewById(R.id.captured_image);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         mCamera = getCameraInstance();
         mCamera.setDisplayOrientation(180);
@@ -65,7 +111,6 @@ public class MainActivity extends AppCompatActivity implements JNIListener {
         preview.addView(mPreview);
     }
 
-
     @Override
     protected void onPause(){
         mDriver.close();
@@ -73,6 +118,23 @@ public class MainActivity extends AppCompatActivity implements JNIListener {
         releaseMediaRecorder();
         releaseCamera();
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(data.getData());
+        if (requestCode == 101 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            galleryUri = data.getData();
+            Intent intent = new Intent(SC_MainActivity.this, SC_ImageEditActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+
 
     @SuppressLint("HandlerLeak")
     public Handler handler = new Handler(){
@@ -169,20 +231,30 @@ public class MainActivity extends AppCompatActivity implements JNIListener {
         public void onPictureTaken(byte[] data, Camera camera) {
             System.out.println(data);
             Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-            int w = bitmap.getWidth() / 2;
-            int h = bitmap.getHeight() / 2;
+
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
             Matrix mtx = new Matrix();
             mtx.postRotate(180);
             Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+            //Bitmap resultBitmap = grayImage(rotatedBitmap);
 
             if (bitmap == null) {
-                Toast.makeText(MainActivity.this, "Captured image is empty", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SC_MainActivity.this, "Captured image is empty", Toast.LENGTH_SHORT).show();
                 return;
             }
-            capturedImageHolder.setImageBitmap(scaleDownBitmapImage(rotatedBitmap, 600, 600));
 
+            capturedImageHolder.setImageBitmap(scaleDownBitmapImage(rotatedBitmap, mPreview.getWidth(), mPreview.getHeight()));
+            capturedBitmap = scaleDownBitmapImage(rotatedBitmap, mPreview.getWidth(), mPreview.getHeight());
         }
     };
+
+
+
+
+
+
+
 
     private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight) {
         Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
@@ -198,4 +270,17 @@ public class MainActivity extends AppCompatActivity implements JNIListener {
         return  Bitmap.createBitmap(src, 0, 0, src.getWidth()/2, src.getHeight()/2, mtx1, true);
     }
 
+    public Bitmap resizeBitmap(Bitmap src, int width, int height){
+        Matrix mtx1 = new Matrix();
+        return  Bitmap.createBitmap(src, 0, 0, width, height, mtx1, true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(capturedImageHolder != null){
+            capturedImageHolder.setImageBitmap(null);
+        }
+        else
+            super.onBackPressed();
+    }
 }
